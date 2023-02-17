e8cbace75455db68a68681c227e4e20d804e1a36
nnmmaz <nnmmaz_@naver.com>
2023-02-17 12:08:18 +0900

    nav, home, social, data done

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className="home__social">
+        <a href="https://www.instagram.com/oh_1012_/" className="home__social-icon" target="_blank">
+            <i class="uil uil-instagram"></i>
+        </a>
+        <a href="https://twitter.com/HAE_S00_" className="home__social-icon" target="_blank">
+            <i class="uil uil-twitter-alt"></i>
+        </a>
+        <a href="https://github.com/ose1012" className="home__social-icon" target="_blank">
+            <i class="uil uil-github-alt"></i>
+        </a>
+    </div>
+  )
+}
+
+export default Social
